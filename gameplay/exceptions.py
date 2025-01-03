99fa19d8edb7281c48120aa0dd36b1f569590f29
draniki-lover <kshmankova@gmail.com>
2025-01-03 15:55:36 +0300

    Update exceptions.py

--- gameplay/exceptions.py
+++ gameplay/exceptions.py
@@ -1,6 +1,20 @@
-class GameOverError(Exception):
-    """Raised when the game ends due to collision."""
-    pass
-class InvalidMoveError(Exception):
-    """Raised when the player tries to move in an invalid direction."""
-    pass
+class InactivityException(Exception):
+    """
+    Исключение для случая, когда игрок бездействует слишком долго
+    """
+    def __init__(self, message="Игрок бездействовал слишком долго."):
+        super().__init__(message)
+
+class SnakeTooLongException(Exception):
+    """
+    Исключение для случая, когда змейка заполняет всё поле
+    """
+    def __init__(self, message="Змейка заполнила всё игровое поле."):
+        super().__init__(message)
+
+class BoarderCollision(Exception):
+    """
+    Исключение для случая столкновения змейки с границой поля
+    """
+    def __init__(self, message = "Змейка врезалась в границу поля!"):
+        super().__init__(message)
