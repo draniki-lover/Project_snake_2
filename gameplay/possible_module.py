8ff951ba1e643adc355b553fffce3695f9832eb8
draniki-lover <kshmankova@gmail.com>
2025-01-03 15:55:05 +0300

    Update possible_module.py

--- gameplay/possible_module.py
+++ gameplay/possible_module.py
@@ -1,5 +1,4 @@
 import turtle
-import time
 
 class Scoreboard:
     def __init__(self, snake_name, position):
@@ -22,13 +21,3 @@ class Scoreboard:
         self.score += 10
         self.pen.clear()
         self.pen.write(f"{self.snake_name}: {self.score}", align="center", font=("Arial", 15, "normal"))
-
-    def game_over(self):
-        """
-        Выводит сообщение о завершении игры
-        """
-        self.pen.goto(0, 0)
-        self.pen.clear()
-        self.pen.pendown()
-        self.pen.write(f"Game Over! {self.snake_name}  Score: {self.score}", align="center", font=("Arial", 24, "normal"))
-        time.sleep(2)
